05c5000e56e8a74e4793312050b7809ac004d072
Rychu <ryrycharizard@outlook.com>
2020-07-17 01:21:15 -0600

    initial buttons added

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-$(document).ready(function(){
+$(document).ready(function () {
+
+    
+
 
     var animeButton = [
-        "Goku","Edward Elric","Itachi Uchiha", "Kakashi Hatake", "Ichigo kurosaki",
-    ]
+        "Goku", "Edward Elric", "Itachi Uchiha", "Kakashi Hatake", "Ichigo kurosaki",
+    ];
+
+    console.log(animeButton.length);
+
+    function makeButton(theArray,btnClass) {
+        for (var i = 0; i< theArray.length; i++){
+            var deButt = $('<input/>').attr({ type: 'button', value: theArray[i],  }); 
+            $("#button-area").append(deButt);
+        };
+
+    };
+
+   
+
+    
+
+    makeButton(animeButton);
+  
 
 });
