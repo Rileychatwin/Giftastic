80ea227996ab3852fc8409b8b2d5fd03e7373ad0
Rychu <ryrycharizard@outlook.com>
2020-07-20 15:40:46 -0600

    Some buttone function added

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 $(document).ready(function () {
-
-    
-
-
     var animeButton = [
         "Goku", "Edward Elric", "Itachi Uchiha", "Kakashi Hatake", "Ichigo kurosaki",
     ];
+    var deButt = "";
+    var queryURL = "";
+    var results = "";
 
-    console.log(animeButton.length);
-
-    function makeButton(theArray,btnClass) {
-        for (var i = 0; i< theArray.length; i++){
-            var deButt = $('<input/>').attr({ type: 'button', value: theArray[i],  }); 
+    function makeButton(theArray,) {
+        for (var i = 0; i < theArray.length; i++) {
+            deButt = $("<button>");
             $("#button-area").append(deButt);
+            deButt.addClass("btn-class");
+            deButt.text(theArray[i]);
+            deButt.data("dataType", theArray[i]);
         };
-
     };
-
-   
-
-    
+    $(document).on("click", ".btn-class", function () {
+        var title = $(this).data("dataType");
+        console.log(title);
+        queryURL =  "http://api.giphy.com/v1/gifs/search?q=" + title + "&/api_key=LkQOIjb9aDhFotnM2tpzgIsISGF8sHOT&limit=10";
+    });
 
     makeButton(animeButton);
-  
-
 });
