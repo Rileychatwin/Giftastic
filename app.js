1ea229745c8a549109b2e6414fd18d70c328f241
Rychu <ryrycharizard@outlook.com>
2020-07-22 01:31:16 -0600

    Added js for appending gifs to page as well as stop and start animation functions

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     var animeButton = [
-        "Goku", "Edward Elric", "Itachi Uchiha", "Kakashi Hatake", "Ichigo kurosaki",
+        "Goku", "Edward Elric", "Itachi Uchiha", "Kakashi Hatake", "Ichigo Kurosaki",
     ];
     var deButt = "";
     var queryURL = "";
@@ -18,7 +18,52 @@ $(document).ready(function () {
     $(document).on("click", ".btn-class", function () {
         var title = $(this).data("dataType");
         console.log(title);
-        queryURL =  "http://api.giphy.com/v1/gifs/search?q=" + title + "&/api_key=LkQOIjb9aDhFotnM2tpzgIsISGF8sHOT&limit=10";
+        queryURL =  "https://api.giphy.com/v1/gifs/search?api_key=LkQOIjb9aDhFotnM2tpzgIsISGF8sHOT&q=" + title + "&limit=10&offset=0&rating=g&lang=en";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+
+        .then(function(response){
+            results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var animeDiv = $("<div class='mx-auto'>");
+
+                var rating = $("<p>").text("Rated: " + results[i].rating.toUpperCase());
+
+                var animeIMG = $("<img>");
+                animeIMG.attr('src', results[i].images.fixed_height_still.url);
+                animeIMG.attr('data-still', results[i].images.fixed_height_still.url);
+                animeIMG.attr('data-animate', results[i].images.fixed_height.url);
+                animeIMG.attr('data-state', 'still');
+                animeIMG.addClass("gif");
+
+                $(animeDiv).append(animeIMG,rating);
+                $("#gif-button").prepend(animeDiv);
+
+            }
+
+        });
+
+        $(document).on("click", ".gif", function () {
+            var state = $(this).attr("data-state");
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+            } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+        });
+
+        $("#gif-buton").on("click", function(event){
+            event.preventDefault();
+            var newBtn = $("#gif-input").val();
+            console.log(newBtn);
+            
+        })
     });
 
     makeButton(animeButton);
